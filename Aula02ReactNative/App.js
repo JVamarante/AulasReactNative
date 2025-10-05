94527e50680fb6b6c0946ba001a6979aacac474b
JVamarante <joaovamarante@gmail.com>
2025-10-05 15:55:35 -0300

    commit

---
+++ Aula02ReactNative/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CartaoPerfil from './src/components/CartaoPerfil'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+      <CartaoPerfil />
+    </View>
+  )
+}
+
+export default App
